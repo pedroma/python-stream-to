6314f8c29fe722ed58cacb3b7d5ff8b028775908
Pedro Araújo <pedromcaraujo@gmail.com>
2013-01-11 17:20:37 +0000

    Initial commit and doc

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 python-stream-to
 ================
+
+For now there are hard-coded IP's and device UUID's. This will change in the future. For dependencies you need:
+
+sudo apt-get install python-requests python-twisted
+
+Twisted is used to serve the file you want to stream to your final device. The protocol used is UpnP. This is in a testing phase but the final idea is to have a nautilus script so you can send any media file (audio,image,video) to a remote device.
+
+Also if I figure out how to make python's SimpleHTTPServer create threads I will use because using twisted for this is just overkill.
+
---
+++ send_to.py
@@ -0,0 +1,81 @@
+import sys, requests, urllib2, subprocess, os
+
+AVTRANSPORT_URI = "http://127.0.0.1:58609/AVTransport/97281f3d-39b4-57f4-e8b3-956f1beb3a7c/control.xml"
+
+TWISTED_SERVE_PATH = os.path.expanduser("~/.config/twisted")
+PIDFILE = "%s/twistd.pid"%TWISTED_SERVE_PATH
+
+HEADERS = {
+    "Content-type":"text/xml;charset=\"utf-8\"",
+    "Connection": "Keep-Alive",
+    "Soapaction":"", # set this before making the request
+}
+
+STOP_PAYLOAD = """
+<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<s:Envelope s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
+    <s:Body>
+        <u:Stop xmlns:u="urn:schemas-upnp-org:service:AVTransport:1">
+            <InstanceID>0</InstanceID>
+        </u:Stop>
+    </s:Body>
+</s:Envelope>
+"""
+
+SET_FILE_PAYLOAD = """
+<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<s:Envelope s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
+    <s:Body>
+        <u:SetAVTransportURI xmlns:u="urn:schemas-upnp-org:service:AVTransport:1">
+            <InstanceID>0</InstanceID>
+            <CurrentURI>http://127.0.0.1:8000/%s</CurrentURI>
+            <CurrentURIMetaData></CurrentURIMetaData>
+        </u:SetAVTransportURI>
+    </s:Body>
+</s:Envelope>
+"""
+
+PLAY_PAYLOAD = """
+<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<s:Envelope s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
+    <s:Body>
+        <u:Play xmlns:u="urn:schemas-upnp-org:service:AVTransport:1">
+            <InstanceID>0</InstanceID>
+            <Speed>1</Speed>
+        </u:Play>
+    </s:Body>
+</s:Envelope>
+"""
+
+def start_twisted():
+    if not os.path.exists(TWISTED_SERVE_PATH):
+        os.makedirs(TWISTED_SERVE_PATH)
+    subprocess.call(["/usr/bin/twistd","--pidfile",PIDFILE,"web","--path",TWISTED_SERVE_PATH,"--port","8000"])
+
+def main(play_file):
+    play_file_base = os.path.basename(play_file)
+    if os.access(PIDFILE,os.F_OK):
+        pidfile = open(PIDFILE,"r")
+        pid = pidfile.readline()
+        pidfile.close()
+        if os.path.exists("/proc/%s"%pid):
+            print "Twisted is running"
+        else:
+            start_twisted()
+    else:
+        start_twisted()
+
+    if not os.path.islink("%s%s"%(TWISTED_SERVE_PATH,play_file_base)):
+        os.symlink(os.path.abspath(play_file), "%s/%s"%(TWISTED_SERVE_PATH,play_file_base))
+
+    HEADERS.update({"Soapaction":"\"urn:schemas-upnp-org:service:AVTransport:1#Stop\""})
+    requests.post(AVTRANSPORT_URI, headers=HEADERS,data=STOP_PAYLOAD) # STOP playing
+    HEADERS.update({"Soapaction":"\"urn:schemas-upnp-org:service:AVTransport:1#SetAVTransportURI\""})
+    requests.post(AVTRANSPORT_URI, headers=HEADERS,data=SET_FILE_PAYLOAD%urllib2.quote(play_file_base))
+    HEADERS.update({"Soapaction":"\"urn:schemas-upnp-org:service:AVTransport:1#Play\""})
+    requests.post(AVTRANSPORT_URI, headers=HEADERS,data=PLAY_PAYLOAD)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1])
+
